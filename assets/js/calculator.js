671c570501715230625d8605b1af6307d82ae29b
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-09-26 16:44:15 -0700

    finalized function for output display
    added switch

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -8,9 +8,53 @@ function updateOutput()
 
 function addOutput(arg)
 {
-	if (arg == "one")
-	{
-		currentOutput += " 1";
+	switch (arg) {
+		case "zero":
+			currentOutput += " 1";
+			break;	
+		case "one":
+			currentOutput += " 1";
+			break;
+		case "two":
+			currentOutput += " 2";
+			break;
+		case "three":
+			currentOutput += " 3";
+			break;
+		case "four":
+			currentOutput += " 4";
+			break;
+		case "five":
+			currentOutput += " 5";
+			break;
+		case "six":
+			currentOutput += " 6";
+			break;
+		case "seven":
+			currentOutput += " 7";
+			break;
+		case "eight":
+			currentOutput += " 8";
+			break;
+		case "nine":
+			currentOutput += " 9";
+			break;
+		case "add":
+			currentOutput += " +";
+			break;
+		case "subtract":
+			currentOutput += " -";
+			break;
+		case "multiply":
+			currentOutput += " x";
+			break;
+		case "divide":
+			currentOutput += " /";
+			break;
+		case "equals":
+			// evaluate output
+			break;
+			
 	}
 	updateOutput();
 }
