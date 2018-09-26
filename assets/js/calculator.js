d37258e70c084b70d6f4f08a3f272acc7efb4ec4
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-09-26 16:40:36 -0700

    added functions to append numbers from buttons to output

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,1 +1,20 @@
-// JavaScript Document
+var currentOutput = "";
+
+
+function updateOutput()
+{
+	$("#output").text(currentOutput);
+}
+
+function addOutput(arg)
+{
+	if (arg == "one")
+	{
+		currentOutput += " 1";
+	}
+	updateOutput();
+}
+			
+$( "#one" ).click(function() {
+	addOutput("one");
+});
